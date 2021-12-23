cb6eb4505861251a16802860be1ba87637daa821
Eric Lopez Clavero <eric.lopez@agilogy.com>
2021-12-23 18:15:57 +0100

    Initial commit with htt4ps skeleton

---
+++ src/main/scala/io/github/locl95/smashtools/Main.scala
@@ -0,0 +1,8 @@
+package io.github.locl95.smashtools
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    SmashtoolsServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
